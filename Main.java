dfe42b3da0446bce9612689647c814d4a4ea9394
tgshore97 <47032536+tgshore97@users.noreply.github.com>
2019-02-04 13:22:26 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,11 @@
 public class Main {
+    //Array is set to 60 as that is the current grid size and large enough to show evolution
     public static int arraySize = 60;
     public static int[][] grid = new int[arraySize][arraySize];
-    public static int[][] grid2 = new int[arraySize][arraySize] ;
     public static int[][] neighs = new int[arraySize][arraySize] ;
 
+
     public static void main(String[] args) {
-        evolution.run(arraySize, grid);
+        evolution.run(arraySize);
     }
 }
