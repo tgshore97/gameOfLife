618ce981e4540694596fe7393ce334f6780dc3f5
tgshore97 <47032536+tgshore97@users.noreply.github.com>
2019-02-04 12:41:45 +0000

    Update display.java

--- display.java
+++ display.java
@@ -1,21 +1,29 @@
 import java.awt.Graphics;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 class display extends JPanel {
-
-    public display(){
+    public display(int[][] grid){
         JFrame frame = new JFrame();
         panel panel = new panel();
 
         frame.setTitle("Game of Life");
         frame.setSize(600, 600);
         frame.setLocationRelativeTo(null);
-
         frame.setLayout(new BorderLayout());
 
-
         JButton button = new JButton("Evolve");
+
+       button.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                evolution.step(grid, Main.grid2, Main.neighs, Main.arraySize);
+                panel.evolveScreen(panel.getGraphics(), grid);
+            }
+        });
+
         button.setBounds(100,100,140, 40);
         frame.add(button, BorderLayout.SOUTH);
         frame.add(panel, BorderLayout.CENTER);
@@ -24,53 +32,33 @@ class display extends JPanel {
         frame.setResizable(false);
     }
 
-
-    public static void createDisplay() {
-        display display = new display();
-
-
+    public static void createDisplay(int[][] grid) {
+        display display = new display(grid);
     }
 
 }
 
 // create a panel that you can draw on.
 class panel extends JPanel {
-    public void paint(Graphics g) {
+
+    public void paint(Graphics g, int[][] grid) {
         for (int i = 10; i < 600; i += 10){
             g.drawLine(0, i, 600, i);
         }
         for (int j = 10; j < 600; j += 10){
             g.drawLine(j, 0, j, 600);
         }
-
-
-
+        for (int i = 0; i < Main.arraySize; i++){
+            for (int j = 0; j < Main.arraySize; j++){
+                if (grid[i][j] == 1){ //SET TO 0 to test array values
+                    g.fillOval(i*10+2, j*10+2, 5, 5);
+                }
+            }
+        }
     }
 
-    evolving step = new evolving();
-
-    /*
-    public void repaintCells(Graphics g, int arraySize, int[][] grid){
-        for (int i = 0; i < arraySize; i++){
-            for (int j = 0; j < arraySize; j++){
-                if (grid[i][j] == 1){
-                    g.drawOval(i + 5, j + 5, 2, 2);
-                }
-            }
-        }
-    }*/
-
-}
-
-
-class evolving extends evolution {
-    public void evolving(Graphics g, int arraySize, int[][] grid) {
-        for (int i = 0; i < arraySize; i++) {
-            for (int j = 0; j < arraySize; j++) {
-                if (grid[i][j] == 1){
-                    g.drawOval(i + 5, j + 5, 2, 2);
-                }
-            }
-        }
+    public void evolveScreen(Graphics g, int[][] grid) {
+        g.clearRect(0, 0, getWidth(), getHeight());
+        paint(g, grid);
     }
 }
